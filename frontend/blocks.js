76227d064b2e3984171fe2ccd1a2eb3ed1905987
Alexander <148774981+psaxys@users.noreply.github.com>
2026-01-20 22:08:13 +0300

    Add Deep Sleep and WiFi Connect blocks

--- frontend/blocks.js
+++ frontend/blocks.js
@@ -1,6 +1,39 @@
+// Определение блока Deep Sleep
+Blockly.Blocks['esp32_deep_sleep'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Уйти в Deep Sleep");
+    this.appendValueInput("SECONDS")
+        .setCheck("Number")
+        .appendField("на");
+    this.appendDummyInput()
+        .appendField("сек.");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(false); // После глубокого сна код не выполняется
+    this.setColour(290);
+    this.setTooltip("ESP32 уснет и проснется через указанное время");
+  }
+};
+
+// Генератор кода C++ для Deep Sleep
+Blockly.JavaScript['esp32_deep_sleep'] = function(block) {
+  var seconds = Blockly.JavaScript.valueToCode(block, 'SECONDS', Blockly.JavaScript.ORDER_ATOMIC) || '10';
+  
+  // Код для настройки таймера и ухода в сон
+  var code = `
+  Serial.println("Ухожу в сон...");
+  esp_sleep_enable_timer_wakeup(${seconds} * 1000000ULL); 
+  esp_deep_sleep_start();
+  `;
+  return code;
+};
+
+// Блок WiFi (с полями ввода)
 Blockly.Blocks['esp32_wifi_connect'] = {
   init: function() {
-    this.appendDummyInput().appendField("WiFi Connect");
+    this.appendDummyInput().appendField("Подключить WiFi");
+    this.appendDummyInput().appendField("SSID:").appendField(new Blockly.FieldTextInput("My_Network"), "SSID");
+    this.appendDummyInput().appendField("PASS:").appendField(new Blockly.FieldTextInput("12345678"), "PASS");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(230);
@@ -8,17 +41,8 @@ Blockly.Blocks['esp32_wifi_connect'] = {
 };
 
 Blockly.JavaScript['esp32_wifi_connect'] = function(block) {
-  return '#include <WiFi.h>\nvoid setup_wifi() { WiFi.begin("SSID", "PASS"); }\n';
-};
-
-// Генератор для Deep Sleep
-Blockly.JavaScript['esp32_deep_sleep'] = function(block) {
-  var seconds = Blockly.JavaScript.valueToCode(block, 'SECONDS', Blockly.JavaScript.ORDER_ATOMIC) || '10';
-  
-  // ESP32 использует микросекунды, поэтому умножаем на 1 000 000
-  var code = `
-  esp_sleep_enable_timer_wakeup(${seconds} * 1000000ULL);
-  esp_deep_sleep_start();
-  `;
-  return code;
+  var ssid = block.getFieldValue('SSID');
+  var pass = block.getFieldValue('PASS');
+  Blockly.JavaScript.definitions_['include_wifi'] = '#include <WiFi.h>';
+  return `WiFi.begin("${ssid}", "${pass}");\nwhile (WiFi.status() != WL_CONNECTED) { delay(500); }\n`;
 };
